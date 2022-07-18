fc57a96ebfb25db0719873125894788b70bc1ea7
Vincius Silva <vinilrsilv@gmail.com>
2022-07-18 09:48:03 -0300

    Using NGRX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+
+import { AddItemAction } from './store/actions/shopping.actions';
+import { AppState } from './store/models/app-state.model';
+import { ShoppingItem } from './store/models/shopping-item.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ngrx-shopping-list';
+export class AppComponent implements OnInit {
+  newShoppingItem: ShoppingItem = { id: '', name: '' };
+
+  constructor(
+    private store: Store<AppState>,
+    public shoppingItems$: Observable<Array<ShoppingItem>>
+  ) {}
+
+  ngOnInit(): void {
+    this.shoppingItems$ = this.store.select((store) => store.shopping);
+  }
+
+  addItem() {
+    this.newShoppingItem.id = uuid();
+
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+
+    this.newShoppingItem = { id: '', name: '' };
+  }
 }
---
+++ src/app/store/actions/shopping.actions.ts
@@ -0,0 +1,18 @@
+import { Action } from '@ngrx/store';
+import { ShoppingItem } from '../models/shopping-item.model';
+
+export enum ShoppingActionTypes {
+    ADD_ITEM = '[SHOPPING] Add Item',
+    ADD_ITEM_SUCCESS = '[SHOPPING] Add Item Success',
+    ADD_ITEM_FAILURE = '[SHOPPING] Add Item Failure',
+}
+
+export class AddItemAction implements Action {
+    readonly type = ShoppingActionTypes.ADD_ITEM;
+
+    constructor(public payload: ShoppingItem){
+
+    }
+}
+
+export type ShoppingAction = AddItemAction;
